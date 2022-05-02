4f641e6a098a4a24191225337abb625bfd9d5c03
lucasmatheus001 <lucasmatheussm22@gmail.com>
2022-05-01 22:37:46 -0300

    [AC-2]: Adicionando controller de professional profile e model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "home#index"
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+  }
 
   resources :users, only: %i[show] do
     resources :addresses
