4cdabe0a0950fd3fc1c657dc4866726803871cb2
lucasmatheus001 <lucasmatheussm22@gmail.com>
2022-05-04 23:22:29 -0300

    [AC-2]: Adicionando Pundit para politica e controle de acesso

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '3.1.1'
 gem 'devise'
 gem 'enumerize'
 
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.5'
 # Use postgresql as the database for Active Record
@@ -57,3 +58,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem "pundit", "~> 2.2"
--- app/controllers/professional_profiles_controller.rb
+++ app/controllers/professional_profiles_controller.rb
@@ -5,10 +5,16 @@ class ProfessionalProfilesController < ApplicationController
     #     @professional_profile = ProfessionalProfille.find(params[:id])
     # end
 
+    # def index
+    #     @professional_profiles = ProfessionalProfile.all
+    # end
+
     def edit
+        authorize @professional_profile
     end
 
     def update
+        authorize @professional_profile
         if current_user.professional_profile.update(professional_profile_params)
             redirect_to user_professional_profile_path(current_user.professional_profile), notice: "Your profile was successfully updated"
         else
@@ -20,6 +26,7 @@ class ProfessionalProfilesController < ApplicationController
     end
 
     def destroy
+        authorize @professional_profile
         @professional_profile.barber_shop_images.find(params[:id])
         @professional_profile.barber_shop_images.push
         # @professional_profile.barber_shop_images.purge
